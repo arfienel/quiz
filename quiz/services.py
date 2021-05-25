1e32bf7e961f88993959e5c84cf62366503ebac6
makek2 <intrershaman@gmail.com>
2021-05-25 12:24:15 +0400

    v0

---
+++ quiz/services.py
@@ -0,0 +1,13 @@
+from .dto import ChoiceDTO, QuestionDTO, QuizDTO, AnswerDTO, AnswersDTO
+from typing import List
+
+
+class QuizResultService():
+    def __init__(self, quiz_dto: QuizDTO, answers_dto: AnswersDTO):
+        self.quiz_dto = quiz_dto
+        self.answers_dto = answers_dto
+
+    def get_result(self) -> float:
+
+        pass
+        # your code here
